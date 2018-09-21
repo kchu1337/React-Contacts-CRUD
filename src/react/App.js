e149918d08f6ca3e11a0f92276a52a01704b0550
Kevin Chu <kevinchu1337@gmail.com>
2018-09-21 02:31:40 -0400

    Updated UI

--- src/react/App.js
+++ src/react/App.js
@@ -12,28 +12,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Contacts Manager</h1>
+          <h2 className="App-subtitle">A web application to create, read, update, and delete contacts. Made in React </h2>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save Fto reload.
+          To update or delete a contact, double click the any part of the row of the contact and click on the update
+          or delete button.
         </p>
-<ContactsTable/>
+        <ContactsTable/>
 
       </div>
     );
   }
 
 }
-/*const Modal = ({ handleClose, show, children }) => {
-  const showHideClassName = show ? 'modal display-block' : 'modal display-none';
-  console.log(showHideClassName);
-  return (
-    <div className={showHideClassName}>
-      <section className="modal-main">
-        {children}
-        <button onClick={handleClose}>close</button>
-      </section>
-    </div>
-  );
-};*/
 export default App;
